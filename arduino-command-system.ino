1157344e7792490efa971f90e44a72584f96f403
boothinator <boothinator@users.noreply.github.com>
2020-10-22 11:09:19 -0400

    Track current command

--- arduino-command-system.ino
+++ arduino-command-system.ino
@@ -1,3 +1,7 @@
+// Keep track of the current command using a single character to represent the command
+// Use the nul character '\0' when there is no command
+char currentCommand = '\0';
+
 void setup() {
   // put your setup code here, to run once:
 
